e4f16681bf8ebfbc96f1d8f1071a6873fbaba8b6
DarkAngelGR <asavvo01@gmail.com>
2020-09-14 11:04:54 +0000

    pyxis: Add AVB flags
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,6 +219,11 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/pyxis/BoardConfigVendor.mk
